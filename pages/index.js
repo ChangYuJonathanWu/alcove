7f5bcaa241ce6027e56fd23b6b33bbf75a35f321
Jonathan Wu <changyujonathanwu@gmail.com>
2023-04-23 18:08:07 -0400

    color themes

--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,54 @@ import 'react-image-shadow/assets/index.css';
 
 const amita = Amita({ weight: ['400', '700'], subsets: ['latin'] })
 
+const theme1 = {
+  bgColor:'#FFF9DE',
+  logoColor: "#FF6969",
+  textColor: "#FF6969",
+  buttonColor: '#FF6969',
+  buttonTextColor: 'white'
+}
+
+const theme2 = {
+  bgColor:'#FDE2F3',
+  logoColor: "#2A2F4F",
+  textColor: "#2A2F4F",
+  buttonColor: '#917FB3',
+  buttonTextColor: 'white'
+}
+
+const theme3 = {
+  bgColor:'#7C9070',
+  logoColor: "#FEE8B0",
+  textColor: "#FEE8B0",
+  buttonColor: '#F97B22',
+  buttonTextColor: 'white'
+}
+
+const theme4 = {
+  bgColor:'#7C9070',
+  logoColor: "white",
+  textColor: "white",
+  buttonColor: '#F97B22',
+  buttonTextColor: 'white'
+}
+
+const theme5 = {
+  bgColor:'#E86A33',
+  logoColor: "#F2E3DB",
+  textColor: "#F2E3DB",
+  buttonColor: '#41644A',
+  buttonTextColor: 'white'
+}
+
 export default function Home() {
-  const claimButtonStyle = {backgroundColor: '#643A6B', textTransform: 'none', borderRadius: '15px', padding: '1rem 2rem'}
-  const backgroundColor = '#B0A4A4'
-  const logoColor = "#5F264A"
-  const textColor ="#643A6B"
+  const theme = theme4;
+  const claimButtonStyle = {backgroundColor: theme.buttonColor, color: theme.buttonTextColor, textTransform: 'none', borderRadius: '15px', padding: '1rem 2rem'}
+  const backgroundColor = theme.bgColor
+  const logoColor = theme.logoColor
+  const textColor = theme.textColor
+
+  
   return (
     <>
       <Head>
@@ -35,9 +78,8 @@ export default function Home() {
             <Typography className={amita.className} style={{fontWeight: 700, color: logoColor}} variant="h1">alcove</Typography>
           </Stack>
           <Typography variant="h1" style={{color: textColor, textAlign:"center", marginBottom: "1rem"}}>Share everything you love - <br/> all in one place.</Typography>
-        
-          <ImageShadow  width={220} style={{marginBottom: "2rem"}} shadowHover={true} src={"/emily.png"} />
-
+  
+          <ImageShadow shadowBlur={25} width={220} style={{marginBottom: "2rem",  transform: "perspective(400px) rotateY(-3deg) rotateX(5deg)"}} shadowHover={true} src={"/emily.png"} />
           <TextField 
                     InputProps={{
                       startAdornment: <InputAdornment sx={{ fontSize: '1rem', marginRight:'0.12rem'}} position="start">alcove.place/</InputAdornment>,
