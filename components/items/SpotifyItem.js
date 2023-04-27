49e311a5f51f449381e3d2efe8b89c074c1bbea3
Jonathan Wu <changyujonathanwu@gmail.com>
2023-04-26 20:25:37 -0400

    zinmdex on skeleton

--- components/items/SpotifyItem.js
+++ components/items/SpotifyItem.js
@@ -10,7 +10,7 @@ export default function SpotifyItem({ item }) {
     return (
         <ListItem key={itemId} sx={{paddingBottom: 0, paddingTop: 0}}>
             {
-                loading && <Skeleton height={92}/>
+                loading && <Skeleton height={92} style={{zIndex: 100}}/>
             }
             <iframe src={uri} width="100%" height="92" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" onLoad={()=>setLoading(false)}></iframe>
         </ListItem>
