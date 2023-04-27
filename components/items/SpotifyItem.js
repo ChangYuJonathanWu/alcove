d61fbe90e84948fa7885e45b6ffb9eeb490cb75b
Jonathan Wu <changyujonathanwu@gmail.com>
2023-04-26 21:32:34 -0400

    another attempt at loader

--- components/items/SpotifyItem.js
+++ components/items/SpotifyItem.js
@@ -9,7 +9,7 @@ export default function SpotifyItem({ item }) {
     const { itemId, uri } = item
     return (
         <ListItem key={itemId} sx={{ paddingBottom: 0, paddingTop: 0 }}>
-            {loading && <Skeleton height={92} style={{ width: "100%", position: "absolute", zIndex: "100", opacity: 1 }} />}
+            {loading && <Skeleton height={92} style={{ width: "100%", opacity: 1}} />}
             <iframe src={uri} width="100%" height="92" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" onLoad={() => setLoading(false)}></iframe>
         </ListItem>
     )
