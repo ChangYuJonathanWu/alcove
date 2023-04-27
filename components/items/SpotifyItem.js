445e23aa275faec695258d8c9e23e190f5ce1ce3
Jonathan Wu <changyujonathanwu@gmail.com>
2023-04-26 20:21:52 -0400

    spotify skeleton load

--- components/items/SpotifyItem.js
+++ components/items/SpotifyItem.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ListItem from '@mui/material/ListItem';
 
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
 export default function SpotifyItem({ item }) {
+    const [loading, setLoading] = useState(true)
     const { itemId, uri } = item
     return (
         <ListItem key={itemId} sx={{paddingBottom: 0, paddingTop: 0}}>
-            <iframe src={uri} width="100%" height="92" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            {
+                loading && <Skeleton height={92}/>
+            }
+            <iframe src={uri} width="100%" height="92" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy" onLoad={()=>setLoading(false)}></iframe>
         </ListItem>
     )
 }
