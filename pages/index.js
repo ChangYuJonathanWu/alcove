a6d21f09cc0a5264fc806b3ae5a753bc3d765c75
Jonathan Wu <changyujonathanwu@gmail.com>
2023-04-27 23:25:25 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -86,7 +86,7 @@ export default function Home() {
 
   const desktopLayout = (
     <Stack style={{marginTop: "4rem"}} direction="row" spacing={isLarge ? 10 : 6} alignItems="start" justifyContent="start">
-      <Hero desktop={true} width={isLarge ? 270 : 250}/>
+      <Hero desktop={true} width={isLarge ? 300 : 250}/>
       <Stack style={{marginTop: "5rem"}} spacing={3}>
         <CallToAction textColor={textColor} highlightColor={theme.textColor} textAlign="start" fontSize={isLarge ? "3.2rem" : "2.5rem"}/>
         <SignUp signupState={signupState} setSignupState={setSignupState} desktop={!isTabletOrMobile} claimButtonStyle={claimButtonStyle} />
@@ -104,7 +104,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main style={{ zIndex: -1, backgroundColor, minHeight: '100vh', width: "100%", position: "fixed" }}>
+      <main style={{ zIndex: -1, backgroundColor, minHeight: '100vh', width: "100%" }}>
         <Stack alignItems="center">
           <Stack direction="row" spacing={0.5} alignItems="center" style={{ padding: "1rem", marginTop: "0.5rem" }}>
             <FoundationIcon style={{ color: theme.buttonColor }} />
