3cf697f2e52e5a43476bbd993486a0afd0c16375
Jonathan Wu <changyujonathanwu@gmail.com>
2023-05-01 00:44:53 -0400

    disable skew

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ImageShadow from 'react-image-shadow';
 import 'react-image-shadow/assets/index.css';
 export default function Hero({width=220, desktop}) {
-    const perspective = desktop ? "perspective(400px) rotateY(3deg) rotateX(5deg)" : "perspective(400px) rotateY(-3deg) rotateX(3deg)"
+    const perspective = "none" // desktop ? "perspective(400px) rotateY(3deg) rotateX(5deg)" : "perspective(400px) rotateY(-3deg) rotateX(3deg)"
   return (
     <ImageShadow shadowBlur={25} width={width} style={{ marginBottom: "2rem", transform: perspective }} shadowHover={desktop} src={"/emily.png"} />
   )
