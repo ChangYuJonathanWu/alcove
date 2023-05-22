e5688cf6f94ac097d37d5958dea5b0d7b48407a0
Jonathan Wu <changyujonathanwu@gmail.com>
2023-05-22 02:24:45 -0400

    clean

--- components/Profile.js
+++ components/Profile.js
@@ -2,33 +2,17 @@ import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { Button, Typography } from '@mui/material';
-
-import ListSubheader from '@mui/material/ListSubheader';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import ExpandMore from '@mui/icons-material/ExpandMore';
 import { buildProfileItems } from './items/buildItems';
 
 import ProfileHeader from './ProfileHeader';
 import AlcoveProfileLogo from '@/components/AlcoveProfileLogo';
 
-import jonathan_user from '../examples/jonathan.json'
-import jiwonkang_user from '../examples/jiwon.json'
-import example_user from '../examples/example.json'
-
 import { useAuthContext } from "@/context/AuthContext";
 import { signOut, getAuth } from "firebase/auth";
 
 import { montserrat } from './fonts';
-
-const PAPER_COLOR = 'rgba(255, 255, 255, 0.8)'
-const MAX_WIDTH = "600px"
 
 
 export default function Profile({ user }) {
