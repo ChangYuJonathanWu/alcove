65e9501175aea2213cfdada225233fb2262a2a58
Jonathan Wu <changyujonathanwu@gmail.com>
2023-06-19 23:33:47 -0400

    wait for load

--- cypress/e2e/profile.cy.js
+++ cypress/e2e/profile.cy.js
@@ -31,6 +31,7 @@ describe('Profile', () => {
     it('Can open a restaurant list and render posts', () => {
         // Open up list
         cy.visit('http://localhost:3000/jHak91janUhqmOakso')
+        cy.wait(3000)
         cy.get("#background-photo").should('exist')
         cy.contains("sushi restaurants").click()
         cy.contains("sushi restaurants").should('exist')
