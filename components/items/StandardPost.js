87104c94f9f4fc21d3ab30f8e809b34cf5a8ccc9
Jonathan Wu <changyujonathanwu@gmail.com>
2023-06-19 23:52:33 -0400

    .

--- components/items/StandardPost.js
+++ components/items/StandardPost.js
@@ -26,20 +26,25 @@ export default function StandardPost({ item, editMode = false, setPostToEdit }) 
                 {image && <Avatar variant="square" sx={{ width: '100%', height: '100%', margin: 'auto' }} src={image} style={{ marginRight: "1rem", borderRadius: '5px' }} />}
                 <Stack direction="row" style={{ width: "100%" }} alignContent="space-between" justifyContent="space-between">
                     <Stack>
-                        <Stack direction="row" spacing={1} alignItems={"center"}>
-                            <Typography variant="h4">
-                            {uri ? <Link variant="inherit" color="inherit" href={uri} underline="none" target="_blank" rel="noreferrer"><b>{title}</b></Link> : <b>{title}</b>}
-                            </Typography>
-                            
-                            {uri && <OpenInNewIcon style={{ width: "1rem" }} />}
-                        </Stack>
+
+                        <Typography variant="h4">
+                            {uri ?
+                                <Link variant="inherit" color="inherit" href={uri} underline="none" target="_blank" rel="noreferrer">
+                                    <Stack direction="row" spacing={1} alignItems={"center"}>
+                                        <b>{title}</b>
+                                        <OpenInNewIcon style={{ width: "1rem" }} />
+                                    </Stack>
+                                </Link>
+                                : <b>{title}</b>}
+                        </Typography>
+
                         {subtitle && <Typography variant="body2" fontSize="0.8rem">{`${subtitle}`}</Typography>}
                         {captionToUse && <Typography variant="caption">{captionToUse}</Typography>}
                     </Stack>
                     {editMode && <EditNoteIcon onClick={onEditClick} />}
                 </Stack>
 
-            </Stack>
-        </ListItem>
+            </Stack >
+        </ListItem >
     )
 }
