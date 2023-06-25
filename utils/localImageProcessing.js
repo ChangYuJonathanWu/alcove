bac029a67b7531b077565c1c15f62f93390ab55f
Jonathan Wu <changyujonathanwu@gmail.com>
2023-06-24 23:56:34 -0400

    .

--- utils/localImageProcessing.js
+++ utils/localImageProcessing.js
@@ -2,7 +2,7 @@ import imageCompression from 'browser-image-compression';
 
 export const compressImage = async (imageFile) => {
     const options = {
-        maxSizeMB: 4,
+        maxSizeMB: 3.7,
         useWebWorker: true
     }
     const compressedFile = await imageCompression(imageFile, options);
