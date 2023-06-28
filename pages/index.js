71878c1c2d5e02695c23cea7f6d83d783062ca3d
Jonathan Wu <changyujonathanwu@gmail.com>
2023-06-27 20:07:10 -0400

    move admin page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,6 @@
 import React, { useState } from 'react'
 import Home from '@/components/home/Home'
-import Dashboard from '@/components/dashboard/Dashboard'
-import { useAuthContext } from "@/context/AuthContext";
 
 export default function Main() {
-  const { user } = useAuthContext()
-
-  return  user ? <Dashboard/> : <Home/>
+  return  <Home/>
 }
