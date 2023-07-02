8d714f13168e08adc499f440ce151dcbf3844af5
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-01 20:03:00 -0400

    .

---
+++ pages/api/cron.js
@@ -0,0 +1,8 @@
+export default async function handler(req, res) {
+    // call home page endpoint
+    const homePage = await fetch('www.alcove.place')
+    console.log('Home Page Status: ', homePage.status)
+    const profile = await fetch('www.alcove.place/healthcheck_test_endpoint')
+    console.log('Profile Status: ', profile.status)
+    return res.status(200).json({ success: homePage.Status === 200 && profile.Status === 200 });
+}
