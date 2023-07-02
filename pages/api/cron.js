4255ea821f9d86c5d46729f5d175d7b04de73a5a
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-01 20:22:06 -0400

    fix cron job urls

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
     // call home page endpoint
-    const homePage = await fetch('www.alcove.place')
+    const homePage = await fetch('https://www.alcove.place')
     console.log('Home Page Status: ', homePage.status)
-    const profile = await fetch('www.alcove.place/healthcheck_test_endpoint')
+    const profile = await fetch('https://www.alcove.place/healthcheck_test_endpoint')
     console.log('Profile Status: ', profile.status)
     return res.status(200).json({ success: homePage.Status === 200 && profile.Status === 200 });
 }
