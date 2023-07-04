21216908a3d21796d3d451bc66fcd7eaad4ac1fe
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-03 20:14:58 -0400

    layout

--- components/profile/ProfileHeader.js
+++ components/profile/ProfileHeader.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Head from 'next/head'
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, Grid } from '@mui/material';
 
 import Avatar from '@mui/material/Avatar';
 
@@ -16,6 +16,14 @@ import Image from 'next/image';
 
 const PAPER_COLOR = 'rgba(255, 255, 255, 0.8)'
 
+const IconGridItem = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+
+    )
+}
 export default function ProfileHeader({ user, setEditMode, ownerSignedIn }) {
     const { title, description, handle, photo, social_links } = user;
     const { instagram, facebook, bereal, snapchat, tiktok, twitter, reddit, linkedin } = social_links
@@ -30,32 +38,32 @@ export default function ProfileHeader({ user, setEditMode, ownerSignedIn }) {
                     {handle && <Typography variant="subtitle1">{`@${handle}`}</Typography>}
 
                     <Typography variant="body">{description}</Typography>
-                    {hasSocialLinks && <Stack direction="row">
-                        {instagram && <IconButton id="instagram-bio-link" href={`https://www.instagram.com/${instagram}`} target="_blank">
+                    {hasSocialLinks && <Grid style={{maxWidth: "180px"}} columns={4} container direction="row" spacing={0} justifyContent="center" alignItems={"center"}>
+                        {instagram && <IconGridItem><IconButton id="instagram-bio-link" href={`https://www.instagram.com/${instagram}`} target="_blank">
                             <InstagramIcon />
-                        </IconButton>}
-                        {tiktok && <IconButton id="tiktok-bio-link" href={`https://www.tiktok.com/${tiktok}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {tiktok && <IconGridItem><IconButton id="tiktok-bio-link" href={`https://www.tiktok.com/@${tiktok}`} target="_blank">
                             <Image priority={true} src="/social_icons/tiktok-icon.svg" width={20} height={20} alt="TikTok logo" />
-                        </IconButton>}
-                        {bereal && <IconButton id="bereal-bio-link" href={`https://www.bereal.com/${bereal}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {bereal && <IconGridItem> <IconButton id="bereal-bio-link" href={`https://www.BeRe.al/${bereal}`} target="_blank">
                             <Image priority={true} src="/social_icons/bereal-icon.svg" width={20} height={20} alt="BeReal logo" />
-                        </IconButton>}
-                        {snapchat && <IconButton id="snapchat-bio-link" href={`https://www.snapchat.com/${snapchat}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {snapchat && <IconGridItem><IconButton id="snapchat-bio-link" href={`https://www.snapchat.com/add/${snapchat}`} target="_blank">
                             <Image priority={true} src="/social_icons/snapchat-icon.svg" width={20} height={20} alt="Snapchat logo" />
-                        </IconButton>}
-                        {facebook && <IconButton id="facebook-bio-link" href={`https://www.facebook.com/${facebook}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {facebook && <IconGridItem><IconButton id="facebook-bio-link" href={`https://www.facebook.com/${facebook}`} target="_blank">
                             <FacebookIcon />
-                        </IconButton>}
-                        {twitter && <IconButton id="twitter-bio-link" href={`https://www.twitter.com/${twitter}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {twitter && <IconGridItem><IconButton id="twitter-bio-link" href={`https://www.twitter.com/${twitter}`} target="_blank">
                             <TwitterIcon />
-                        </IconButton>}
-                        {reddit && <IconButton id="reddit-bio-link" href={`https://www.reddit.com/${reddit}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {reddit && <IconGridItem><IconButton id="reddit-bio-link" href={`https://www.reddit.com/user/${reddit}`} target="_blank">
                             <RedditIcon />
-                        </IconButton>}
-                        {linkedin && <IconButton id="linkedin-bio-link" href={`https://www.linkedin.com/${linkedin}`} target="_blank">
+                        </IconButton></IconGridItem>}
+                        {linkedin && <IconGridItem> <IconButton id="linkedin-bio-link" href={`https://www.linkedin.com/in/${linkedin}`} target="_blank">
                             <LinkedInIcon />
-                        </IconButton>}
-                    </Stack>}
+                        </IconButton></IconGridItem>}
+                    </Grid>}
                     {ownerSignedIn && <Button style={{ textTransform: 'none', color: 'black', borderColor: 'gray', fontSize: '0.8rem', width: '100%' }} variant="outlined" onClick={() => setEditMode(true)}>Edit Profile</Button>}
                 </Stack>
             </Paper>
