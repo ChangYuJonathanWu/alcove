1aa4532f2f5dce242a31ab5211703c45b33ead71
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-05 21:33:59 -0400

    Static Loading Logo (#67)

--- components/DefaultLoader.js
+++ components/DefaultLoader.js
@@ -5,14 +5,15 @@ import { PulseLoader } from "react-spinners";
 import Navbar from "./home/Navbar";
 import { Stack } from "@mui/material";
 import Image from "next/image";
+import Logo from "../public/favicon.svg"
 
 
 export default function DefaultLoader() {
     return (
         <div style={{ height: '100vh', backgroundColor: 'white', width: '100%' }}>
             <Stack style={{height: '100%'}} justifyContent={"center"} spacing={1} alignItems="center">
-                <Image priority={true} src="favicon.svg" width={250} height={80} alt="Alcove logo" />
-                <PulseLoader color="#F97B22" size={10} />
+                <Image priority={true} src={Logo} width={250} height={80} alt="Alcove logo" />
+                <PulseLoader color="#F97B22" size={12} />
             </Stack>
 
         </div>
