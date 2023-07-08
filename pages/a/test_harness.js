0d2645c59405394e300c9df5a5c9e3f8c0c97f88
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-08 18:41:05 -0400

    hacker

--- pages/a/test_harness.js
+++ pages/a/test_harness.js
@@ -7,14 +7,18 @@ import { Stack } from '@mui/material';
 
 export default function TestHarness() {
 
-  return (
-    <div>
-        <Stack>
-        <Link href="/239jsdfk9Q2jjsk_no_spotify">Profile: No Spotify</Link>
-        <Link href="/demo_the_artist">The Artist: demo1</Link>
-        <Link href="/demo_the_foodie">The Foodie: demo2</Link>
-        </Stack>
-        
-    </div>
-  )
+    return (
+        <div>
+            <Stack>
+                <Link href="/239jsdfk9Q2jjsk_no_spotify">Profile: No Spotify</Link>
+                <Link href="/demo_the_artist">The Artist: demo1</Link>
+                <Link href="/demo_the_foodie">The Foodie: demo2</Link>
+                <Link href="/demo_the_outdoors">The Foodie: demo3</Link>
+                <Link href="/demo_the_baker">The Baker: demo4</Link>
+                <Link href="/demo_the_hacker">The Hacker: demo5</Link>
+                
+            </Stack>
+
+        </div>
+    )
 }
