33374e2f4c10d8432d75fec75df0d4f115138b8b
Jonathan Wu <changyujonathanwu@gmail.com>
2023-07-26 23:51:41 -0400

    remove transition on loader

--- components/DefaultLoader.js
+++ components/DefaultLoader.js
@@ -6,19 +6,15 @@ import Navbar from "./home/Navbar";
 import { Stack } from "@mui/material";
 import Image from "next/image";
 import Logo from "../public/favicon.svg"
-import PageTransition from "./PageTransition";
 
 
 export default function DefaultLoader() {
     return (
-        <PageTransition>
-            <div key="default-loader" style={{ zIndex: 10, height: '100vh', backgroundColor: 'white', width: '100%' }}>
-                <Stack style={{ height: '100%' }} justifyContent={"center"} spacing={1} alignItems="center">
-                    <Image priority={true} src={Logo} width={250} height={80} alt="Alcove logo" />
-                    <PulseLoader color="#F97B22" size={12} />
-                </Stack>
-
-            </div>
-        </PageTransition>
+        <div key="default-loader" style={{ zIndex: 10, height: '100vh', backgroundColor: 'white', width: '100%' }}>
+            <Stack style={{ height: '100%' }} justifyContent={"center"} spacing={1} alignItems="center">
+                <Image priority={true} src={Logo} width={250} height={80} alt="Alcove logo" />
+                <PulseLoader color="#F97B22" size={12} />
+            </Stack>
+        </div>
     )
 }
