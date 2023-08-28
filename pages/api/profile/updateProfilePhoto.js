108d77c08f4d31b7dc4308d4ebbf9e94b7364a20
Jonathan Wu <changyujonathanwu@gmail.com>
2023-08-28 20:11:59 +0800

    increase profile image compression

--- pages/api/profile/updateProfilePhoto.js
+++ pages/api/profile/updateProfilePhoto.js
@@ -77,7 +77,7 @@ async function handler(req, res) {
             const imagePath = imageFile.path;
             let compressedImage
             try {
-                compressedImage = await resizeImage(imagePath, 600, 600)
+                compressedImage = await resizeImage(imagePath, 300, 300)
             } catch (e) {
                 console.error(e)
                 Sentry.captureException(e)
