b4f1be308863908666f0b090961086d95e3c2488
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-02 19:38:06 +0800

    login button works but breaks all other user flow screens

--- components/home/Navbar.js
+++ components/home/Navbar.js
@@ -4,6 +4,7 @@ import Logo from '@/components/home/static/alcove-logo-dark.png'
 import { AppBar, Stack, Button, Box } from '@mui/material'
 import { amita } from '../fonts'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 import React, { useState, useEffect } from 'react'
 
@@ -15,14 +16,22 @@ const theme = {
     buttonTextColor: 'white'
 }
 
+
+
 export default function Navbar({ mobile }) {
     const backgroundColor = theme.bgColor
     const logoColor = theme.logoColor
     const textColor = theme.textColor
+    const router = useRouter();
+
+    const onLogin = () => {
+        router.replace('/login')
+      }
+    
     return (
         <Stack direction="row" alignItems="center" justifyContent="space-between" width="100%" >
             <Image src={Logo}  height={mobile ? 35 : 50} alt="Alcove logo" />
-            <Button variant="contained" style={{textTransform: 'none', backgroundColor: 'black', borderRadius: '0.5rem'}}>
+            <Button onClick={onLogin} variant="contained" style={{textTransform: 'none', backgroundColor: 'black', borderRadius: '0.5rem'}}>
                 Log in
             </Button>
         </Stack>
