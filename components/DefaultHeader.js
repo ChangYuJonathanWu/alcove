a2f9e5373f22884adb56bf86079d5079c235b265
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-08 06:31:15 -0700

    refactor heax

---
+++ components/DefaultHeader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function DefaultHeader({title}) {
+    return (
+        <Head>
+            <title>{title}</title>
+            <meta name="description" content="Your link-in-bio to share everything you love." />
+            <meta property="og:title" content={title} />
+            <meta
+                property="og:description"
+                content="Your link-in-bio to share everything you love."
+            />
+            <meta
+                property="og:image"
+                content="/social-share.png"
+            />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/images/favicon.svg" />
+        </Head>
+    )
+}
