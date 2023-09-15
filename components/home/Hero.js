3219d730ec630bdcf4a5628fe55a457eaed82802
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-14 22:01:52 -0700

    .

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -40,7 +40,7 @@ export default function Hero({ desktop }) {
     <Marquee pauseOnHover={false} >
       {
         importedProfilesToShow.map((profile, idx) => {
-          return <Image key={idx} height={500} style={{ margin: `${desktop ? "2.5rem" : "3rem"} 1rem 4rem 1rem`, borderRadius: '1rem', boxShadow: desktop ? desktopBoxShadow : mobileBoxShadow }} alt="Example profile" src={profile} />
+          return <Image key={idx} height={500} style={{ margin: `${desktop ? "2.5rem" : "3.5rem"} 1rem 4rem 1rem`, borderRadius: '1rem', boxShadow: desktop ? desktopBoxShadow : mobileBoxShadow }} alt="Example profile" src={profile} />
         })
       }
     </Marquee>
