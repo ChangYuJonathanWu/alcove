27574816bb06fda54dfee3453f0e0f55a701bc05
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-16 15:11:35 -0700

    .

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -8,13 +8,13 @@ import Image from 'next/image';
 import { Stack } from '@mui/material';
 
 import TheBaker from '@/components/home/static/thebaker_rounded.png'
-import TheArtist from '@/components/home/static/theartist_rounded.png'
-import TheFoodie from '@/components/home/static/thefoodie_rounded.png'
+import TheArtist from '@/components/home/static/corrine_elle_artist.png'
 import TheHacker from '@/components/home/static/thehacker_rounded.png'
 import TheOutdoors from '@/components/home/static/theoutdoors_rounded.png'
 import TheGeneralist from '@/components/home/static/thegeneralist_rounded.png'
-import TheReader from '@/components/home/static/thereader_rounded.png'
-import TheTraveller from '@/components/home/static/thetraveller_rounded.png'
+import TheReader from '@/components/home/static/chris_akachi_reader.png'
+import TheTraveller from '@/components/home/static/eli_santos_traveler.png'
+import TheFoodie from '@/components/home/static/jessie_shu_foodie.png'
 import Avatar from '@mui/material';
 
 import Marquee from "react-fast-marquee";
@@ -23,13 +23,13 @@ import Marquee from "react-fast-marquee";
 export default function Hero({ desktop }) {
   const importedProfilesToShow = [
     TheArtist,
-    TheOutdoors,
-    TheTraveller,
     TheGeneralist,
+    TheOutdoors,
     TheFoodie,
-    TheReader,
     TheHacker,
+    TheTraveller,
     TheBaker,
+    TheReader,
   ]
 
   const mobileBoxShadow = 'rgba(0, 0, 0, 0.25) 0px 24px 25px, rgba(0, 0, 0, 0.12) 0px -12px 24px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px'
