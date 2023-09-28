d0e76a90a48f79e65edfa121d5f36f93094cc0e8
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-27 22:53:19 -0700

    Organize onboarding admin dashboard

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -15,7 +15,8 @@ export default function Dashboard() {
     const [signups, setSignups] = useState([])
     const [user, authLoading, authError] = useAuthState(auth)
     const [trigger, setTrigger] = useState(false)
-    const [ status, setStatus ]   = useState(null)
+    const [status, setStatus] = useState(null)
+    const [editMode, setEditMode] = useState(false)
     useEffect(() => {
         const loadSignups = async () => {
             const result = await protectedApiCall('/api/admin/user_management', "GET")
@@ -28,10 +29,10 @@ export default function Dashboard() {
 
     }, [user, trigger])
 
-    const onOnboardAttempt = async (signupId, attempt, email, handle, increment=true) => {
+    const onOnboardAttempt = async (signupId, attempt, email, handle, increment = true) => {
         setStatus(null)
         const result = await protectedApiCall(`/api/admin/user_management/attempt_onboard`, "POST", JSON.stringify({ email, handle, signupId, attemptNumber: attempt, increment }))
-        if(result.status !== 200) {
+        if (result.status !== 200) {
             setStatus("Error sending onboarding email, status: " + result.status)
         } else {
             setStatus("Onboarding email sent for ID: " + signupId)
@@ -49,8 +50,8 @@ export default function Dashboard() {
 
     const onTestOnboardingEmail = async () => {
         setStatus(null)
-        const result = await protectedApiCall(`/api/admin/user_management/attempt_onboard`, "POST", JSON.stringify({test: true}))
-        if(result.status !== 200) {
+        const result = await protectedApiCall(`/api/admin/user_management/attempt_onboard`, "POST", JSON.stringify({ test: true }))
+        if (result.status !== 200) {
             setStatus("Error sending test onboarding email, status: " + result.status)
         } else {
             setStatus("Test onboarding email sent")
@@ -60,20 +61,60 @@ export default function Dashboard() {
     const buildSignups = () => {
         const completedSignups = signups.filter((signup) => signup.complete).reverse()
         const incompleteSignups = signups.filter((signup) => !signup.complete).reverse()
+        const toOnboard = incompleteSignups.filter((signup) => !signup.attempts)
+        const toFollowUp = incompleteSignups.filter((signup) => signup.attempts === 1).reverse()
+        const toFinal = incompleteSignups.filter((signup) => signup.attempts === 2).reverse()
         return (
 
             <>
-                <Typography variant="h3">Incomplete Signups</Typography>
+                <Typography variant="h3" style={{ fontWeight: '600' }}>Onboarding Signups</Typography>
                 <Stack spacing={1}>
-                    {incompleteSignups.map((signup) => {
+                    {toOnboard.map((signup) => {
+                        const { email, _id, handle, attempts = 0 } = signup
+
+                        return (
+                            <Stack key={_id} direction="row" alignItems="center" spacing={2} style={{ padding: '1rem', borderBottom: '1px solid black' }}>
+                                <Typography>{email} - {handle} - {_id} </Typography>
+                                <Button style={{ backgroundColor: attempts === 0 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 0)}>Onboard</Button>
+                                <Typography> | </Typography>
+                                {editMode && <Button onClick={() => onComplete(_id)}>Complete</Button>}
+                                {editMode && <Button onClick={() => onDelete(_id)} style={{ color: 'red' }}>Delete</Button>}
+                            </Stack>
+                        )
+                    }
+                    )}
+                </Stack>
+                <Typography variant="h3">Follow Up Signups</Typography>
+                <Stack spacing={1}>
+                    {toFollowUp.map((signup) => {
                         const { email, _id, handle, attempts = 0 } = signup
 
                         return (
                             <Stack key={_id} direction="row" alignItems="center" spacing={2}>
                                 <Typography>{_id} - {email} - {handle}</Typography>
-                                <Button style={{backgroundColor: attempts === 0 ? "white" : "gray"}} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 0)}>Onboard</Button>
-                                <Button style={{backgroundColor: attempts === 1 ? "white" : "gray"}}onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 1)}>Follow Up</Button>
-                                <Button style={{backgroundColor: attempts === 2 ? "white" : "gray"}} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 2)}>Final</Button>
+                                {/* <Button style={{ backgroundColor: attempts === 0 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 0)}>Onboard</Button>
+                                <Button style={{ backgroundColor: attempts === 1 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 1)}>Follow Up</Button>
+                                <Button style={{ backgroundColor: attempts === 2 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 2)}>Final</Button> */}
+                                <Typography> | </Typography>
+                                {editMode && <Button onClick={() => onComplete(_id)}>Complete</Button>}
+                                {editMode && <Button onClick={() => onDelete(_id)} style={{ color: 'red' }}>Delete</Button>}
+                                <Typography>{handle}</Typography>
+                            </Stack>
+                        )
+                    }
+                    )}
+                </Stack>
+                <Typography variant="h3">Final Follow Up Signups</Typography>
+                <Stack spacing={1}>
+                    {toFinal.map((signup) => {
+                        const { email, _id, handle, attempts = 0 } = signup
+
+                        return (
+                            <Stack key={_id} direction="row" alignItems="center" spacing={2}>
+                                <Typography>{_id} - {email} - {handle}</Typography>
+                                <Button style={{ backgroundColor: attempts === 0 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 0)}>Onboard</Button>
+                                <Button style={{ backgroundColor: attempts === 1 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 1)}>Follow Up</Button>
+                                <Button style={{ backgroundColor: attempts === 2 ? "white" : "gray" }} onClick={() => onOnboardAttempt(_id, attempts, email, handle, attempts === 2)}>Final</Button>
                                 <Typography> | </Typography>
                                 <Button onClick={() => onComplete(_id)}>Complete</Button>
                                 <Button onClick={() => onDelete(_id)} style={{ color: 'red' }}>Delete</Button>
@@ -123,9 +164,13 @@ export default function Dashboard() {
             </Head>
 
             <main>
-                <Stack>
+                <Stack style={{ margin: '1rem' }}>
                     {status && <Typography color="red">{status}</Typography>}
-                    <Button onClick={onTestOnboardingEmail} variant={"contained"}>Send Test Onboarding</Button>
+                    <Stack direction="row" spacing={2} style={{ padding: '1rem' }}>
+                        <Button onClick={onTestOnboardingEmail} variant={"contained"}>Send Test Onboarding</Button>
+                        <Button onClick={() => setEditMode(!editMode)} variant={"contained"}>Toggle Edit Mode</Button>
+                    </Stack>
+
                     {buildSignups()}
                 </Stack>
 
