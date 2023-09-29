a6c8f30144b87400b964c2b8366371bef5690753
Jonathan Wu <changyujonathanwu@gmail.com>
2023-09-29 15:15:15 -0700

    .

--- pages/api/signup/index.js
+++ pages/api/signup/index.js
@@ -41,7 +41,7 @@ export default async function handler(req, res) {
       }
 
       const data = {
-        success: signupSuccess,
+        success: !!signupSuccess,
         errors
       }
       return res.status(200).json(data);
