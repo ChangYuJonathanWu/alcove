0e3b2ff7b587bfcf95c2611a0a2bf76f4207a19c
Jonathan Wu <changyujonathanwu@gmail.com>
2023-10-06 21:09:44 -0700

    Rearrange Posts Modal enhancements (#140)

--- components/profile/RearrangePosts/CompactItemView.js
+++ components/profile/RearrangePosts/CompactItemView.js
@@ -3,10 +3,10 @@ import { Modal, Stack, Box, Button, Typography, TextField, Avatar } from '@mui/m
 import { truncateString } from '@/utils/formatters';
 export default function CompactItemView({item}) {
     let { title, subtitle, caption, image } = item;
-
+    const photoOnly = !title && !subtitle && !caption && image
     return (
-        <Stack direction="row" spacing={2}>
-            <Avatar variant="square" style={{ height: "5rem", width: "5rem", borderRadius: '1rem' }} src={image} />
+        <Stack direction="row" spacing={2} justifyContent={photoOnly ? 'center' : 'start'} style={{width: '100%'}}>
+            <Avatar variant="square" style={{ height: photoOnly ? '8rem' : "5rem", width: photoOnly ? 'auto' : "5rem", borderRadius: '1rem' }} src={image} />
             <Stack>
                 {title ? <Typography style={{ fontWeight: 600 }}>{truncateString(title, 30)}</Typography> : <div style={{ height: '1.2rem' }}></div>}
                 {subtitle ? <Typography variant="subtitle1">{truncateString(subtitle, 35)}</Typography> : <div style={{ height: '1rem' }}></div>}
