9a6cd5de8d5e63c15dea248c18542b88130f5388
Jonathan Wu <changyujonathanwu@gmail.com>
2023-10-19 22:28:47 -0700

    .

--- components/DefaultLoader.js
+++ components/DefaultLoader.js
@@ -6,6 +6,7 @@ import Navbar from "./home/Navbar";
 import { Stack } from "@mui/material";
 import Image from "next/image";
 import Logo from "../public/favicon.svg"
+import AlcovePulseLoader from "./custom/AlcovePulseLoader";
 
 
 export default function DefaultLoader() {
@@ -13,7 +14,7 @@ export default function DefaultLoader() {
         <div key="default-loader" style={{ zIndex: 10, height: '100vh', backgroundColor: 'white', width: '100%' }}>
             <Stack style={{ height: '100%' }} justifyContent={"center"} spacing={1} alignItems="center">
                 <Image priority={true} src={Logo} width={250} height={80} alt="Alcove logo" />
-                <PulseLoader color="#F97B22" size={12} />
+                <AlcovePulseLoader />
             </Stack>
         </div>
     )
--- components/profile/Profile.js
+++ components/profile/Profile.js
@@ -22,6 +22,8 @@ import ViewAsPublicButton from './ViewAsPublicButton';
 import ThemingModal from './ThemingModal';
 import LogoutButton from './LogoutButton';
 import MenuFAB from './MenuFAB/MenuFAB';
+import PullToRefresh from 'react-simple-pull-to-refresh';
+import AlcovePulseLoader from '../custom/AlcovePulseLoader';
 
 
 export default function Profile({ user, ownerSignedIn = false, mobileApp = false }) {
@@ -55,7 +57,7 @@ export default function Profile({ user, ownerSignedIn = false, mobileApp = false
     const logoutUser = async () => {
         const auth = getAuth();
         await signOut(auth);
-        router.replace('/login')
+        router.replace(mobileApp ? ' /m/login' : '/login')
     }
 
     const canRearrangeItems = itemOrder.length > 1
@@ -65,44 +67,38 @@ export default function Profile({ user, ownerSignedIn = false, mobileApp = false
         onThemeChange: () => setThemeOpen(true),
     }
 
+    const RefreshWrapper = ({ children }) => {
+        return mobileApp ? <PullToRefresh onRefresh={triggerReload} pullingContent={<div></div>} refreshingContent={<div style={{ margin: '1rem' }}><AlcovePulseLoader /></div>} >{children}</PullToRefresh> : children
+    }
+
     //TODO: Error handling on network request, validation
     return (
-        <main>
-
-            <div style={{ height: '100%', minHeight: '100vh', width: '100%', position: "fixed", backgroundColor: '#cfcfcf', alignItems: "center", zIndex: 0 }}>
-
-                {backgroundType == "image" && <Image priority={true} fill={true} src={backgroundUrl} objectFit='cover' id="background-photo" alt="background wallpaper" />}
-
-            </div>
-
-            {ownerSignedIn && <MenuFAB key="menu-fab" profilePhotoUri={photo} handle={handle} clickHandlers={clickHandlers} />}
-
-
-
-            <Stack style={{ marginBottom: "8.5rem" }}>
-
-                <div style={{ zIndex: 1 }}>
-                    <EditBioModal open={editBio} setOpen={setEditBio} user={profileUser} triggerReload={triggerReload} />
-                    <NewItemModal open={newItemOpen} setOpen={setNewItemOpen} triggerReload={triggerReload} />
-                    <RearrangeItemsModal open={reorderItems} setOpen={setReorderItems} user={profileUser} triggerReload={triggerReload} />
-                    <ThemingModal open={themeOpen} setOpen={setThemeOpen} user={profileUser} triggerReload={triggerReload} />
-                    {config.demo_mode && <div style={{ height: "2rem" }}></div>}
-                    <ProfileHeader user={profileUser} setEditMode={setEditBio} ownerSignedIn={ownerSignedIn} mobileApp={mobileApp} />
-                    <ProfileItems user={profileUser} editMode={ownerSignedIn} triggerReload={triggerReload} mobileApp={mobileApp} />
+        <RefreshWrapper>
+            <main>
+                <div style={{ height: '100%', minHeight: '100vh', width: '100%', position: "fixed", backgroundColor: '#cfcfcf', alignItems: "center", zIndex: 0 }}>
+                    {backgroundType == "image" && <Image priority={true} fill={true} src={backgroundUrl} objectFit='cover' id="background-photo" alt="background wallpaper" />}
                 </div>
-
-                {ownerSignedIn &&
-                    <Stack style={{ zIndex: 1 }}>
-                        <NewItemButton key="new-item-button" onClick={() => setNewItemOpen(true)} />
-                        {canRearrangeItems && <RearrangeItemsButton key="rearrange-items-button" onClick={() => setReorderItems(true)} />}
-                    </Stack>}
-
-                <div style={{ zIndex: 1 }}>
-                    {(!config.hide_logo && !ownerSignedIn) && <AlcoveProfileLogo mobileApp={mobileApp} />}
-                </div>
-
-
-            </Stack>
-        </main>
+                {ownerSignedIn && <MenuFAB key="menu-fab" profilePhotoUri={photo} handle={handle} clickHandlers={clickHandlers} />}
+                <Stack style={{ marginBottom: "8.5rem" }}>
+                    <div style={{ zIndex: 1 }}>
+                        <EditBioModal open={editBio} setOpen={setEditBio} user={profileUser} triggerReload={triggerReload} />
+                        <NewItemModal open={newItemOpen} setOpen={setNewItemOpen} triggerReload={triggerReload} />
+                        <RearrangeItemsModal open={reorderItems} setOpen={setReorderItems} user={profileUser} triggerReload={triggerReload} />
+                        <ThemingModal open={themeOpen} setOpen={setThemeOpen} user={profileUser} triggerReload={triggerReload} />
+                        {config.demo_mode && <div style={{ height: "2rem" }}></div>}
+                        <ProfileHeader user={profileUser} setEditMode={setEditBio} ownerSignedIn={ownerSignedIn} mobileApp={mobileApp} />
+                        <ProfileItems user={profileUser} editMode={ownerSignedIn} triggerReload={triggerReload} mobileApp={mobileApp} />
+                    </div>
+                    {ownerSignedIn &&
+                        <Stack style={{ zIndex: 1 }}>
+                            <NewItemButton key="new-item-button" onClick={() => setNewItemOpen(true)} />
+                            {canRearrangeItems && <RearrangeItemsButton key="rearrange-items-button" onClick={() => setReorderItems(true)} />}
+                        </Stack>}
+                    <div style={{ zIndex: 1 }}>
+                        {(!config.hide_logo && !ownerSignedIn) && <AlcoveProfileLogo mobileApp={mobileApp} />}
+                    </div>
+                </Stack>
+            </main>
+        </RefreshWrapper>
     )
 }
